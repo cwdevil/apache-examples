4d6fd26b753022fcd9660a5aabd1ae150a0757f2
Remilia <HinanawiCirno@outlook.com>
2017-09-22 11:46:33 +0800

    init apache metamodel

---
+++ apache-metamodel-examples/src/main/java/me/mushen/examples/metamodel/Test.java
@@ -0,0 +1,13 @@
+package me.mushen.examples.metamodel;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @Desc
+ * @Author Remilia
+ * @Create 2017-09-22
+ */
+public class Test {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Test.class);
+}
